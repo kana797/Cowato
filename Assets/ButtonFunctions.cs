3c41f458feb7060b9908bb39ef106ffa0853c8a0
kana797 <wangzirun68@gmail.com>
2025-02-01 13:34:36 -0500

    add feeeeeed

---
+++ Assets/ButtonFunctions.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void Feed()
+    {
+        Debug.Log("?????");
+    }
+}
