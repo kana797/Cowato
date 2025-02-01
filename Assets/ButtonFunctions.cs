034184dcfc0a1399afe6697b986c0f5a8d93cb3f
kana797 <wangzirun68@gmail.com>
2025-02-01 15:17:36 -0500

    Added Feed, button and animation

--- Assets/ButtonFunctions.cs
+++ Assets/ButtonFunctions.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class ButtonFunctions : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    public Animator CowatoController;
     public void Feed()
     {
         Debug.Log("?????");
+        CowatoController.SetTrigger("Feed");
     }
 }
