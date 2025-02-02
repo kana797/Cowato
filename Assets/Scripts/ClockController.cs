8e88f791f7a7018b7d1db65286a89fee8d93e14c
kana797 <wangzirun68@gmail.com>
2025-02-01 20:37:33 -0500

    timer

--- Assets/Scripts/ClockController.cs
+++ Assets/Scripts/ClockController.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class ClockController : MonoBehaviour
 {
     public GameObject clock;
+    public GameObject timePanel;
+    private TMP_Text buttonText;
+    private float time = 60f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartTimer()
     {
         clock.SetActive(true);
-
+        timePanel.SetActive(true);
+        buttonText = clock.GetComponentInChildren<TMP_Text>();
     }
 }
